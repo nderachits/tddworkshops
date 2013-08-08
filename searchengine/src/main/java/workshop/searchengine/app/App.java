acadd821e6b47184cda672ca103f40c31fbfa735
Mikalai Dzerachyts <Mikalai_Dzerachyts@epam.com>
2013-08-08 18:49:08 +0300

    searchengine mvn project

---
+++ searchengine/src/main/java/workshop/searchengine/app/App.java
@@ -0,0 +1,13 @@
+package workshop.searchengine.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
